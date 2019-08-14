79c65cc41fca5b1009960b12beb2b17b741a4072
Christopher Dent <christopherdent01@gmail.com>
2019-08-14 02:04:29 +0000

    Automatically backed up by Learn

--- spec/02_shoe_spec.rb
+++ spec/02_shoe_spec.rb
@@ -4,10 +4,15 @@ describe 'Shoe' do
     # made aware of the brands of the shoes that are being created?
 
     it 'keeps track of all brands' do
+      
       brands = ["Uggs", "Rainbow"]
+      
+      
       brands.each do |brand|
         Shoe.new(brand)
       end
+
+
 
       brands.each do |brand|
         expect(Shoe::BRANDS).to include(brand)
